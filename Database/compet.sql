cfc086a58740000070f17c979e94572c74158f72
saitokojiro <kazutonight@gmail.com>
2020-10-29 07:38:57 +0100

    -

--- Database/compet.sql
+++ Database/compet.sql
@@ -4,17 +4,34 @@ CREATE DATABASE IF NOT EXISTS compets_management;
 USE compets_management;
 
 DROP TABLE IF EXISTS epreuves;
+DROP TABLE IF EXISTS categorie;
+DROP TABLE IF EXISTS profil;
 DROP TABLE IF EXISTS passages;
 DROP TABLE IF EXISTS resultat;
+
 
 CREATE TABLE IF NOT EXISTS epreuves
 (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(32) NOT NULL,
-    categorie VARCHAR(32) NOT NULL,
-    profil VARCHAR(32) NOT NULL,
+    categorie int(11) NOT NULL,
+    profil int(11) NOT NULL,
     lieu VARCHAR(32) NOT NULL,
     lifeDate DATE NOT NULL
+)Engine=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS categories
+(
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    categorie VARCHAR(32) NOT NULL
+
+)Engine=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS profils
+(
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    profil VARCHAR(32) NOT NULL
+
 )Engine=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS participants
@@ -24,8 +41,8 @@ CREATE TABLE IF NOT EXISTS participants
     nom VARCHAR(32) NOT NULL,
     prenom VARCHAR(32) NOT NULL,
     photo VARCHAR(32) NOT NULL,
-    categorie VARCHAR(32) NOT NULL,
-    profil VARCHAR(32) NOT NULL,
+    categorie int(11) NOT NULL,
+    profil int(11) NOT NULL,
     email VARCHAR(32) NOT NULL UNIQUE,
     date_de_naissance DATE NOT NULL
 )Engine=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
@@ -48,26 +65,61 @@ CREATE TABLE IF NOT EXISTS resultat
 
 INSERT INTO epreuves (nom, categorie, profil, lieu, lifeDate)
 VALUE
-('blog','snow', 'open', 'paris','2019-09-24 16:15:54'),
-('blog','snow', 'close', 'paris','2019-09-24 16:15:54');
+('blog','6', '2', 'paris','2019-09-24 16:15:54'),
+('blog','6', '2', 'paris','2019-09-24 16:15:54');
 
 INSERT INTO resultat (epreuve_id, participant_id, nombre_passage, temps_one, temps_two)
 VALUE
 ('1','1', '2', '15','12');
 
+INSERT INTO categories (categorie)
+VALUE
+("M1"),
+("M2"),
+("M3"),
+("Senior"),
+("V"),
+("Snow"),
+("Nouvelle Glisse NG");
+
+INSERT INTO profils (profil)
+VALUE
+("ASVP : ex-pervenches"),
+("Open: conjoints et/ou enfants"),
+("Gardes-champêtres");
+
 
 
 INSERT INTO participants (photo, nom, prenom, categorie, profil, email, date_de_naissance)
 VALUE
-("flopi.jpg","plopi","flo","snow","open","hello@test.fr", "2019-09-24 16:15:54"),
-("flopi.jpg","plopi","flo","snow","open","hello@tests.fr", "2019-09-24 16:15:54");
+("flopi.jpg","plopi","flo","6","2","hello@test.fr", "2019-09-24 16:15:54"),
+("flopi.jpg","plopi","flo","6","2","hello@tests.fr", "2019-09-24 16:15:54");
 
+-- participants
+ ALTER TABLE participants
+ ADD CONSTRAINT fk_participants_categories
+ FOREIGN KEY (categorie) REFERENCES categories(id);
+ 
+ ALTER TABLE participants
+ ADD CONSTRAINT fk_participants_profils
+ FOREIGN KEY (profil) REFERENCES profils(id);
+ 
+ -- epreuve
+ ALTER TABLE epreuves
+ ADD CONSTRAINT fk_epreuve_categories
+ FOREIGN KEY (categorie) REFERENCES categories(id);
+ 
+ ALTER TABLE epreuves
+ ADD CONSTRAINT fk_epreuve_profils
+ FOREIGN KEY (profil) REFERENCES profils(id);
 
+-- participant
 
  ALTER TABLE resultat
  ADD CONSTRAINT fk_participants_resultat
  FOREIGN KEY (participant_id) REFERENCES participants(id);
  
+ -- epreuves
  -- CREATE INDEX gt_epreuveID ON epreuves (id);
  ALTER TABLE resultat
  ADD CONSTRAINT fk_epreuves_resultat
