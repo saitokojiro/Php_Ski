3374a97d98de603b010fdee6ceb55a7f8e8c88eb
saitokojiro <kazutonight@gmail.com>
2020-10-30 11:55:04 +0100

    add file resultat

---
+++ src/controller/ResultatController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\controller;
+
+namespace App\controller;
+
+use App\controller\TwigConfig;
+use App\model\DatabaseModel;
+use App\repository\ParticipantsRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResultatController extends DatabaseModel
+{
+
+    public function __construct()
+    {
+    }
+
+    public function viewAllParticipant()
+    {
+
+    }
+}
