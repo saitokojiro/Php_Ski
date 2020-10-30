2af6a9d9d983ae6c5371831fcf8bf0eb04938094
saitokojiro <kazutonight@gmail.com>
2020-10-30 17:05:34 +0100

    ajout du contenu csv dans la Database

--- src/controller/ResultatController.php
+++ src/controller/ResultatController.php
@@ -6,6 +6,7 @@ namespace App\controller;
 use App\controller\TwigConfig;
 use App\model\DatabaseModel;
 use App\repository\ParticipantsRepository;
+use App\repository\ResultatRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\controller\CsvController;
@@ -15,20 +16,26 @@ class ResultatController extends DatabaseModel
 {
     public $twig;
     public $uploaded;
+    public $resRepo;
 
     public function __construct()
     {
         $this->twig = new TwigConfig();
         $this->uploaded = new CsvController();
+        $this->resRepo = new ResultatRepository();
     }
 
     public function viewAllParticipant(Request $request, Response $response): Response
     {
         $test = $this->uploaded->csvImportAnotherPage($request);
-        dump($test);
-
+        $url = explode('=', $request->getPathInfo());
+        $this->resRepo->addParticipantsResult($test , $url[1]);
         $contentPage = $this->twig->twig->render('resultatList.html.twig', ['pList' => $test]);
         $response->setContent($contentPage);
         return $response;
+    }
+    public function ParticipantResult()
+    {
+
     }
 }
